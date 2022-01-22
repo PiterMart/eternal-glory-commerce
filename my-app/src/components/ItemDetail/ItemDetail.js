9bb0da54f49e989069caa7fcdd9055bad7268e0e
PiterMart <pitermartingaste@gmail.com>
2022-01-22 15:42:31 -0300

    lots of changes to come

--- my-app/src/components/ItemDetail/ItemDetail.js
+++ my-app/src/components/ItemDetail/ItemDetail.js
@@ -1,90 +1,82 @@
-import { useState, useContext } from 'react'
-import './ItemDetail.css'
-import CartContext   from '../../context/cartContext'
-import NotificationContext from '../../context/NotificationContext'
+import { useState, useContext } from "react";
+import "./ItemDetail.css";
+import CartContext from "../../context/cartContext";
+import NotificationContext from "../../context/NotificationContext";
 
+const InputCount = ({ onConfirm, maxQuantity }) => {
+  const [count, setCount] = useState(0);
 
-const InputCount = ({onConfirm, maxQuantity}) => {
-    const [count, setCount] = useState(0)
-
-    const handleChange = ({target}) => {
-        if(target.value <= maxQuantity && target.value >= 0) {
-            setCount(target.value)
-        }
+  const handleChange = ({ target }) => {
+    if (target.value <= maxQuantity && target.value >= 0) {
+      setCount(target.value);
     }
+  };
 
-    return (
-        <div>
-            <input type='number' onChange={handleChange} value={count}/>
-            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <input type="number" onChange={handleChange} value={count} />
+      <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+    </div>
+  );
+};
 
-const ButtonCount = ({ onConfirm, maxQuantity}) => {
-    const [count, setCount] = useState(0)
+const ButtonCount = ({ onConfirm, maxQuantity }) => {
+  const [count, setCount] = useState(0);
 
-    const increment = () => {
-        if(count < maxQuantity) {
-            setCount(count + 1)
-        }
+  const increment = () => {
+    if (count < maxQuantity) {
+      setCount(count + 1);
     }
+  };
 
-    const decrement = () => {
-        if(count > 0) {
-            setCount(count - 1)
-        }
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
     }
+  };
 
-    return (
-      <div className='counterContainer'>
-          <div className='count'>
-              <button onClick={decrement}>-</button>
-              <span>{count}</span>
-              <button onClick={increment}>+</button>
-              <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
-          </div>
+  return (
+    <div className="counterContainer">
+      <div className="count">
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
+        <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-const ItemDetail = ({ product, inputType = 'input' }) => {
+const ItemDetail = ({ product, inputType = "input" }) => {
+  const Count = inputType === "input" ? InputCount : ButtonCount;
 
-  const Count = inputType === 'input' ? InputCount : ButtonCount
-
-  const {setNotification} = useContext(NotificationContext)
-  const {addItem, } = useContext(CartContext)
+  const { setNotification } = useContext(NotificationContext);
+  const { addItem } = useContext(CartContext);
 
   const addToCart = (count) => {
     addItem(product, count);
-    setNotification('succes', `Agregado al carrito ${count} ` + product?.name)  
-    console.log('se agrego ' + count + ' ' + product?.name)
-  }
+    setNotification("succes", `Agregado al carrito ${count} ` + product?.name);
+    console.log("hello se agrego " + count + " " + product?.name);
+  };
 
-    return (
-        <article className="CardItem">
-            <header className="Header">
-                <h2 className="ItemHeader">
-                    {product?.name}
-                </h2>
-            </header>
-            <picture>
-                <img src={product?.img} alt={product?.name} className="ItemImg"/>
-            </picture>
-            <section>
-                <p className="Info">
-                    {product?.description}
-                </p>
-                <p className="Info">
-                    $ {product?.price}
-                </p>
-            </section>           
-            <footer className='ItemFooter'>
-             <Count onConfirm={addToCart} maxQuantity={product?.stock}/>
-             {/* <Count maxQuantity={product?.stock}/> */}
-            </footer>
-        </article>
-    )
-}
+  return (
+    <article className="CardItem">
+      <header className="Header">
+        <h2 className="ItemHeader">{product?.name}</h2>
+      </header>
+      <picture>
+        <img src={product?.img} alt={product?.name} className="ItemImg" />
+      </picture>
+      <section>
+        <p className="Info">{product?.description}</p>
+        <p className="Info">$ {product?.price}</p>
+      </section>
+      <footer className="ItemFooter">
+        <Count onConfirm={addToCart} maxQuantity={product?.stock} />
+        {/* <Count maxQuantity={product?.stock}/> */}
+      </footer>
+    </article>
+  );
+};
 
-export default ItemDetail
+export default ItemDetail;
--- my-app/src/context/cartContext.js
+++ my-app/src/context/cartContext.js
@@ -1,55 +1,73 @@
-import React, {useState, createContext}from 'react'
+import React, { useState, createContext, useEffect, useCallback } from "react";
 
+const CartContext = createContext([]);
 
-const CartContext = React.createContext([])
+export const CartContextProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const [totalQuantity, setTotalQuantity] = useState(0);
 
-export const CartContextProvider = ({children}) => {
-    const [cart, setCart] = useState([])
-    const [totalQuantity, setTotalQuantity] = useState(0)
-
-    const addItem = (item, quantity) => {
-        console.log(item);
-        const flag = isInCart(item);
-        if (flag) {
-            let repeatedProduct = cart.find(element => element.item === item);
-            repeatedProduct.quantity += quantity;
-            let auxiliarCart = cart.filter(element => element.item !== item);
-            setCart([...auxiliarCart, repeatedProduct])
-        } else {
-            setCart([...cart, {item: item, quantity: quantity}]);
-        }
-        addAll();
-        console.log(flag);  
+  const addItem = (item, quantity) => {
+    const flag = isInCart(item);
+    if (flag) {
+      let repeatedProduct = cart.find((element) => element.item === item);
+      const newObject = {
+        item: repeatedProduct.item,
+        quantity: Number(repeatedProduct.quantity),
+      };
+      newObject.quantity += Number(quantity);
+      let auxiliarCart = cart.filter((element) => element.item !== item);
+      setCart([...auxiliarCart, newObject]);
+    } else {
+      setCart((prevState) => {
+        const result = [
+          ...prevState,
+          { item: item, quantity: Number(quantity) },
+        ];
+        return result;
+      });
     }
-    const isInCart = (item) => {
-        console.log(item);
-        return cart.some(product => product.item === item);
-    }
+    console.log(flag);
+  };
+  const isInCart = (item) => {
+    console.log(item);
+    return cart.some((product) => product.item === item);
+  };
+
+  const memoizedAddAll = useCallback(() => {
     const addAll = () => {
-        console.log('adding')
-        cart.forEach(element => {
-            setTotalQuantity (totalQuantity + element.quantity)
-        })
-    }
-    // const removeItem = (item) => {
+      const totalQuantityArray = cart.map((item) => Number(item.quantity));
+      const totalSum = totalQuantityArray.reduce((a, b) => {
+        return a + b;
+      }, 0);
+      setTotalQuantity(totalSum);
+    };
+    addAll();
+  }, [cart]);
 
-    // }
-    // const cleanCart = () => {
+  useEffect(() => {
+    memoizedAddAll();
+  }, [cart, memoizedAddAll]);
 
-    // }
+  // const removeItem = (item) => {
 
+  // }
+  // const cleanCart = () => {
 
-    return (
-        <CartContext.Provider value={{
-            cart, quantity: {
-                totalQuantity,
-                addItem,
-            },
-            addItem
-        }}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+  // }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart: {
+          totalQuantity,
+          addItem,
+        },
+        addItem,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 
 export default CartContext;
